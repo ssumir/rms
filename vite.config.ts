ae7ed588956f406c76f8a58e63803e4e34aabdb3
ssumir <saifulislam-74@dipti.com.bd>
2025-12-09 10:54:51 +0600

    again update

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
+// vite.config.ts
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  // ⬅️ ADD THIS LINE: Tells Vite to prefix asset URLs with the repository name
+  base: "/rms/", 
+  plugins: [react()],
 })
